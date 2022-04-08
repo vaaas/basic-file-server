73c7a412af994427f8dba759f2a86c2c40cf9e83
Vasileios Pasialiokis <pasivasi@outlook.com>
2022-04-08 17:14:52 +0300

    remove conf.php from build

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ version='1.0.0'
 basename='configuration-server'
 dirname="$basename"_"$version"_amd64
 mkdir -p $dirname/opt/$basename $dirname/etc/systemd/system
-cp -pr --parents conf.php index.php lib start.sh $dirname/opt/$basename
+cp -pr --parents index.php lib start.sh $dirname/opt/$basename
 cp -pr etc/configuration-server.service $dirname/etc/systemd/system
 cp -pr DEBIAN $dirname
 chmod 755 $dirname/DEBIAN
