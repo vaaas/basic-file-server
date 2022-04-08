babd8a6f10b84ae21c1b3bd1e6133d0daea6c7c4
Vasileios Pasialiokis <pasivasi@outlook.com>
2022-04-08 14:30:34 +0300

    add debian build file, systemd service

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ mkdir -p tars conf
 if test "$1" = 'production'
 then
     echo 'starting configuration server'
-    php -S 0.0.0.0:8080 index.php 2> /dev/null
+    php -S 0.0.0.0:8000 index.php 2> /dev/null
     echo 'configuration server killed'
-else php -S 0.0.0.0:8080 index.php
+else php -S 0.0.0.0:8000 index.php
 fi
