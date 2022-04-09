c68ae7164f4973c5f39d3e720b694ea9e517061a
vas <whiterocket@outlook.com>
2022-04-09 06:29:09 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,15 +3,6 @@
 require_once 'lib/response.php';
 require_once 'lib/request.php';
 
-function generateRandomString(int $length = 10): string {
-    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $charactersLength = strlen($characters);
-    $randomString = '';
-    for ($i = 0; $i < $length; $i++)
-        $randomString .= $characters[rand(0, $charactersLength - 1)];
-    return $randomString;
-}
-
 function route(Request $request): Response {
     try {
         if ($request->method !== 'GET') return new MethodNotAllowed();
